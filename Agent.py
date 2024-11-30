c48728a05937ac4befea385a917df876bb35d79d
Rob Pitkin <43574523+rob-pitkin@users.noreply.github.com>
2024-11-30 11:01:34 -0800

    add a simple unit test an neural net

--- Agent.py
+++ Agent.py
@@ -1,11 +1,26 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
 
 
 class PolicyGradientAgent(nn.Module):
     def __init__(self):
         super(PolicyGradientAgent, self).__init__()
-    
+        self.state_dim = 4
+        self.hidden_dim = 64
+        self.action_dim = 2
+        self.ff1 = nn.Linear(self.state_dim, self.hidden_dim)
+        self.ff2 = nn.Linear(self.hidden_dim, self.action_dim)
+        self.activation = nn.ReLU()
+        self.softmax = nn.Softmax(dim=-1)
+
     def forward(self, x):
-        # The forward pass of the policy network
+        x = self.ff1(x)
+        x = self.activation(x)
+        x = self.ff2(x)
+        return self.softmax(x)
+
+
+def train_agent(env, agent, num_episodes):
+    pass
--- testAgent.py
+++ testAgent.py
@@ -1,9 +1,15 @@
 import gymnasium as gym
+import torch
+from Agent import PolicyGradientAgent
+import unittest
 
 
-def main():
+def testCartPole():
     env = gym.make("CartPole-v1", render_mode="human")
     obs, info = env.reset()
+    print(
+        f"Observation space shape: {obs.shape}, action space shape: {env.action_space}"
+    )
 
     episode_over = False
     while not episode_over:
@@ -11,10 +17,26 @@ def main():
             env.action_space.sample()
         )  # sample a random action from the action space
         obs, reward, terminated, truncated, info = env.step(action)
-        print(f"Observation: {obs}, reward: {reward}")
+        print(f"Observation: {obs}, action: {action}, reward: {reward}")
 
         episode_over = terminated or truncated
 
 
+class TestPolicyGradientAgent(unittest.TestCase):
+    def testAgentForward(self):
+        agent = PolicyGradientAgent()
+        env = gym.make("CartPole-v1")
+        obs, info = env.reset()
+        out = agent.forward(torch.tensor(obs))
+        print(out)
+        self.assertTrue(sum(out) == 1.0)
+
+
+def main():
+    # testCartPole()
+    pass
+
+
 if __name__ == "__main__":
     main()
+    unittest.main()
